20062275bc85f0c1b6a4cb382e8efa22874e549d
Kevin Hoang <kevin.hoang199@gmail.com>
2023-04-19 15:26:07 -0400

    Completed project

---
+++ todolist.js
@@ -0,0 +1,47 @@
+// Let's create a simple to-do application! Everyone can use a good to-do list, and this one will also serve to give you a chance to apply some of those JS skills.
+
+// Create a to-do list and it must accomplish the following things:
+
+// Show an unordered list of to-do items
+// Show an input to enter a new to-do
+// Show a button to add a to-do. When the button is clicked:
+// The text from the input box is used to add a list item to the bottom of the list
+// The text from the input box is cleared out.
+// When the user clicks on a list item, it is removed
+ 
+
+// Challenge (Optional)
+
+// When a list item is clicked, cross it out, then remove it after 1 second.
+// Add items into browser storage
+ 
+// Additional Considerations
+
+// While the main focus of this exercise is to test to see your JavaScript, this is also one of your first applications that we can see in the browser. 
+// Do you know what that means?? You can employ some of your handy HTML and CSS skills you have learned thus far. 
+// I would love to see some creative lists and great use of styling to enhance the look and feel of your app.
+
+
+function addTask(item){
+    let newLI = document.createElement('li');
+    newLI.innerHTML = item;
+
+    const toDoList = document.querySelector('ul');
+    toDoList.appendChild(newLI);
+
+    addRemoveTask();
+}
+
+function addRemoveTask(){
+    let eleRemove = document.querySelectorAll('li');
+    for(let i =0;i < eleRemove.length; i++ ){
+        eleRemove[i].addEventListener('click', function(event){eleRemove[i].remove();});
+    }
+}
+
+let taskText = document.getElementById("formID")
+taskText.addEventListener('submit', function(event){
+    event.preventDefault();
+    let addTaskText = document.getElementById('addTask').value;
+    addTask(addTaskText);
+})
